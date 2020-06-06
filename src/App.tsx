c18fee318640510382e681dd53387bc6e4dde38a
Valdir Lourenco <valdir2310@yahoo.com.br>
2020-06-06 00:17:02 -0300

    retirado o header de exemplo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
-  let [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
   return (
     <h1>Hello World</h1>
   );
