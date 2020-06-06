3161af4468d339714a7e8823b64c2ee858170857
Valdir Lourenco <valdir2310@yahoo.com.br>
2020-06-06 01:03:29 -0300

    criacao da pagina Home e inicio da CreatePoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Routes />
   );
 }
 
